dc17cdf9ff6bc7e7aaf6306fca240f0332a40b35
Nguyen Tuan Huy <127500794+huy12C10@users.noreply.github.com>
2024-07-28 20:59:34 +0700

    Update MyConstants.js

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,12 +1,12 @@
 const MyConstants = {
-  DB_SERVER: '',
-  DB_USER: '',
+  DB_SERVER: 'shopping.rzqoj6m.mongodb.net',
+  DB_USER: 'long',
   DB_PASS: '1234',
-  DB_DATABASE: '',
+  DB_DATABASE: 'shoppingonline',
   JWT_SECRET: '1234',
   JWT_EXPIRES: '99999999999999999',
-  EMAIL_USER: 'abc@gmail.com',
-  EMAIL_PASS: ''
+  EMAIL_USER: '',
+  EMAIL_PASS: 'xywivtssfxutuzuq'
 };
 
 module.exports = MyConstants;
